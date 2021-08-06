17a92b2966a6352bf84d4f8978daebbb9167dd8c
Ryan <ryan@ryannd.com>
2021-08-06 11:38:48 -0500

    Added Song model and migration, registered songs app in settings

--- music_library/songs/models.py
+++ music_library/songs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    release_date = models.DateField()
