641701e2af3e01bb59ce15712e8d9c01b5247025
Ryan <ryan@ryannd.com>
2021-08-06 13:42:22 -0500

    Updated status code for SongDetail.put

--- music_library/songs/views.py
+++ music_library/songs/views.py
@@ -32,7 +32,7 @@ class SongDetail(APIView):
         serializer = SongSerializer(song, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
