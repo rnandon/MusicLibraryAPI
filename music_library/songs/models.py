84d93564624627dd5bb272ce345b34023924ebde
Ryan <ryan@ryannd.com>
2021-08-13 11:35:53 -0500

    Added 'genre' to Song class and updated SongSerializer, includes new migration

--- music_library/songs/models.py
+++ music_library/songs/models.py
@@ -5,4 +5,5 @@ class Song(models.Model):
     artist = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
     release_date = models.DateField()
+    genre = models.CharField(max_length=50)
     likes = models.IntegerField(default=0)
--- music_library/songs/serializers.py
+++ music_library/songs/serializers.py
@@ -4,7 +4,7 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'artist', 'album', 'release_date', 'likes']
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre', 'likes']
 
     def like(self, instance):
         instance.likes += 1
